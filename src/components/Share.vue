7c7d3149fb3209f495fe55b6d7df91f3b08fa2e0
Is-Yang <1255786936@qq.com>
2019-02-17 17:30:04 +0800

    first commit

---
+++ src/components/Share.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="share">
+        <nav-bar
+            title="邀请好友赚奖励"
+            left-arrow
+        />
+    </div>
+</template>
+
+<script>
+import { NavBar } from 'vant'
+export default {
+    name: 'share',
+    components: {
+        NavBar
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import ProductList from './components/ProductList'
+import ProductDetail from './components/ProductDetail'
+import Share from './components/Share'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: ProductList
+  }, {
+    path: '/detail',
+    component: ProductDetail
+  }, {
+    path: '/share',
+    component: Share
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
 
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
